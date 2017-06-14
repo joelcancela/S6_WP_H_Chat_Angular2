70656f48a3a8ee60a3c3363d7e67364065ae0231
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2017-06-14 16:54:20 +0200

    Fixed emotes

---
+++ src/shared/constants/emotes.ts
@@ -0,0 +1,3 @@
+export const EMOTES = ["🙂", "😉", "😐", "🙁", "😃", "😛", "💗", "😯"];
+export const REG_EMOTES = [new RegExp(":\\)"), new RegExp(";\\)"), new RegExp(":'\\("), new RegExp(":\\("), new RegExp(":D"),
+  new RegExp(":p"), new RegExp("<3"), new RegExp(":o")];
