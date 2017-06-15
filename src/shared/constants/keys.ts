df160eff7b2b2dec7330fcb7b9ecf9ce1b6d63f0
Marc Jourdes <marcjourdes@free.fr>
2017-06-15 14:02:03 +0200

    Renamed constants to lower case: https://angular.io/guide/styleguide#style-03-02

--- src/shared/constants/keys.ts
+++ src/shared/constants/keys.ts
@@ -1,2 +1,2 @@
-export const OPENWEATHERAPIKEY = "375d0a6cd1f0774238c8efced059f674";
-export const TRANSLATE_KEY = "trnsl.1.1.20170615T081805Z.0e302df0273dbf61.aa1989672146f8df4c4c4cedca0509b21193bed5";
+export const openWeatherAPIKey = "375d0a6cd1f0774238c8efced059f674";
+export const translateKey = "trnsl.1.1.20170615T081805Z.0e302df0273dbf61.aa1989672146f8df4c4c4cedca0509b21193bed5";
